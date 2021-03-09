90d40d882ee242273663574b2cb989bfa288c7e2
CheatCentral <uks9370@edu.kunskapsskolan.se>
2021-03-09 11:59:01 +0000

    KESH V1.0.8

--- main.lua
+++ main.lua
@@ -1,5 +1,8 @@
---KESH V1.0.8
 --[[
+  KESH V1.0.8
+  
+  This is the KESH assembly file, this loads the language engine aswell as the code project to make the stuff run
+
   Do not touch anything in here, it may break all of KESH!
 ]]
 require "codelibrary.Engine" --This loads the external language
